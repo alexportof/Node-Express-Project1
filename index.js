9a0c2f01cb860c30f648423c7b5c799f7f841d3e
Álex Porto <alexportoferreira@hotmail.com>
2019-04-22 19:43:48 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.listen(3000)
