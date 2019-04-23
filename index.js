6a491281b8b1d424f21bec2fda14b4e74e1331da
Álex Porto <alexportoferreira@hotmail.com>
2019-04-23 06:16:35 -0300

    Add redirection in route '/check' and add views major and minor

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ nunjucks.configure('views', {
   watch: true
 })
 app.set('view engine', 'njk')
+
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   return res.render('age')
+})
+
+app.get('/major', (req, res) => {
+  return res.render('major')
+})
+
+app.get('/minor', (req, res) => {
+  return res.render('minor')
+})
+
+app.post('/check', (req, res) => {
+  console.log(req.body)
+
+  if (req.body.age >= 18) {
+    res.redirect('/major')
+  } else {
+    res.redirect('/minor')
+  }
 })
 
 app.listen(3000)
