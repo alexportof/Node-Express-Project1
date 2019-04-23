1116a8daa38d73372e36a68d6808030af75d7f83
Álex Porto <alexportoferreira@hotmail.com>
2019-04-23 12:20:23 -0300

    Add Query Params and project finalization

--- index.js
+++ index.js
@@ -8,29 +8,37 @@ nunjucks.configure('views', {
   watch: true
 })
 app.set('view engine', 'njk')
-
 app.use(express.urlencoded({ extended: false }))
+
+const checkQueryParams = (req, res, next) => {
+  const { age } = req.query
+  if (!age) {
+    return res.redirect('/')
+  }
+  return next()
+}
 
 app.get('/', (req, res) => {
   return res.render('age')
 })
-var age
 
-app.get('/major', (req, res) => {
+app.get('/major', checkQueryParams, (req, res) => {
+  const { age } = req.query
   return res.render('major', { age })
 })
 
-app.get('/minor', (req, res) => {
+app.get('/minor', checkQueryParams, (req, res) => {
+  const { age } = req.query
   return res.render('minor', { age })
 })
 
 app.post('/check', (req, res) => {
   console.log(req.body)
-  age = req.body.age
+  const { age } = req.body
   if (age >= 18) {
-    res.redirect('/major')
+    res.redirect(`/major?age=${age}`)
   } else {
-    res.redirect('/minor')
+    res.redirect(`/minor?age=${age}`)
   }
 })
 
